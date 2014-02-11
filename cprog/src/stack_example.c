9dc434a6903f84f39e5e6d9a14e5412d583d47d2
Phaneendra Chiruvella <hi@pcx.io>
2014-02-11 08:56:16 +0530

    stack_example now uses debug macros and some cleanup

--- cprog/src/stack_example.c
+++ cprog/src/stack_example.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "dbg.h"
 #include "linked_list.h"
 #include "stack.h"
 
@@ -7,22 +9,30 @@
 int main(int argc, char **argv)
 {
      LinkedStack *st = Stack_create();
-     printf("Created an empty stack data structure.\n");
-     
-     st->push(&(st->_head), 1);
-     st->push(&(st->_head), 2);
-     st->push(&(st->_head), 3);
-     st->push(&(st->_head), 4);
-     st->push(&(st->_head), 5);
-     st->push(&(st->_head), 6);
-     printf("Pushed numbers 1-6 onto the stack.\n");
+     check_mem(st);
+     log_info("Created an empty stack data structure.\n");
 
-     printf("Now emptying stack by poping all its elements:\n");
-     for(int i = 1; ; i++) {
-	  printf("element %d is %d.\n", i, st->pop(&(st->_head)));
+     for(int i = 1; i <= 6; i++) {
+	  if(st->push(&(st->_head), i) != 0)
+	       sentinel("Failed to push onto stack.");
+     }
+     log_info("Pushed numbers 1-6 onto the stack.\n");
+
+     log_info("Now emptying stack by poping all its elements:\n");
+     int i, v = 0;
+     for(i = 1; ; i++) {
+	  v = st->pop(&(st->_head));
+	  if(!clean_errno()) {
+	       sentinel("Failed to pop off the stack. Something's wrong.");
+	  }
+
+	  log_info("element %d is %d.\n", i, v);
 	  if(!st->_head)
 	       break;
      }
-
+     if(st) free(st);
      return 0;
+error:
+     if(st) free(st);
+     return -1;
 }
