52b2ba13a3a0a533bcd14f960eab3cf88b7d59a7
Phaneendra Chiruvella <hi@pcx.io>
2014-04-29 14:16:53 +0530

    updates with a working solution for ex1.12 pascals triangle

--- sicp/ch1/ex1.12.scm
+++ sicp/ch1/ex1.12.scm
@@ -1,10 +1,21 @@
-;; element el at position x for nth row
-;; el(x)(n) = 0 if x<1 or x> n+1
-;;          = 1 if x=1 or x=n+1
-;;          = el(x-1)(n-1) + el(x)(n-1)
+;; Calculate an element in pascal's triangle
+;; n -> the row number in the triangle, where
+;; the triangle starts from row 1
+;; i-> the column number in each row, where the
+;; row starts from column number 1
 
-(define (pt-el x n)
-  (cond ((or (> x (+ n 1)) (< x 1)) 0)
-	((or (= x 1) (= x (+ n 1))) 1)
-	(else (+ (pt-el (- x 1) (- n 1))
-		 (pt-el x (- n 1))))))
+(define (pascal n i)
+  (cond ((or (= i 1) (= n i)) 1)
+	(else (+ (pascal (- n 1) (- i 1))
+		 (pascal (- n 1) i)))))
+
+(pascal 4 2)
+(pascal 5 2)
+(pascal 6 2)
+(pascal 6 3)
+(pascal 7 3)
+(pascal 8 3)
+(pascal 9 3)
+(pascal 10 3)
+(pascal 10 4)
+(pascal 10 5)
